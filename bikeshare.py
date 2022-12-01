73003c4dc8d7ac1d83407d0fb83e03d2edd52cae
Bedoor Alharbi <47708221+ibedoor@users.noreply.github.com>
2022-12-01 12:15:02 +0300

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -197,6 +197,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+        print("\nSee you soon :)")
 
 
 if __name__ == "__main__":
